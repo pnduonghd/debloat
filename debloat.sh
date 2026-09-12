eef650f1c74143db50167186fc098e10d8f576e3
git <git@git-scm.com>
2026-09-12 17:16:35 +0700

    1

--- debloat.sh
+++ debloat.sh
@@ -1,12 +1,4 @@
 curl -s https://raw.githubusercontent.com/pnduonghd/debloat/refs/heads/main/packages.txt | while IFS= read -r line || [ -n "$line" ]; do
-    # Loại bỏ khoảng trắng thừa đầu/cuối dòng
-    line=$(echo "$line" | xargs)
-    
-    # Bỏ qua dòng trống hoặc dòng bắt đầu bằng dấu #
-    case "$line" in
-        ""|\#*) continue ;;
-    esac
-
     echo "Đang xử lý: $line"
     service call package 136 s16 "$line" i32 0 i32 0
 done
